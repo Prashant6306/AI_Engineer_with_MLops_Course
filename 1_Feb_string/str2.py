35aa1290f1566ebc5660b75f571e73fbff0060ad
Prashant6306 <parhad.prashant@gmail.com>
2026-02-01 22:35:35 +0530

    commited feb_1

---
+++ 1_Feb_string/str2.py
@@ -0,0 +1,6 @@
+str5=' let\'s play cricket'
+
+print(str5)
+
+str6="let's play cricket"
+print(str6)
---
+++ 30_Jan_Into_to_python/demo.py
@@ -0,0 +1,34 @@
+# #print("Jai Ganesh")
+
+# name="santosh"
+# print(name)
+
+# age=25
+# income=10000.10
+# name="santosh"
+# bool1=True
+
+# a=10
+# A=[1,2,3,4,5]
+# print(type(A))
+# A=10.1
+#B=1.0234+2j
+#print(B)
+# A,B,C=10,10.1,"santosh"
+# print(A)
+# print(B)
+# print(C)
+# a=b=c=d=5
+# print(a,b,c,d)
+# c=True
+# d="santosh"
+# e=[1,2,3,4,5]
+# f=(1,2,3,4,5)
+# g={1,2,3,4,5}
+# h={a:10,b:20}
+
+a=5
+b=3
+c=a**b
+print(c)
+print(id(c))
